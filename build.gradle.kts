010841d45e08c63132a4f152ca38bd9900ef9d95
Vladimir Hromada <vhromada@gmail.com>
2020-10-24 14:27:40 +0200

    Spring Boot 2.3.4.RELEASE, Kotlin logging 1.12.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,16 +4,16 @@ plugins {
 }
 
 group = "com.github.vhromada.project"
-version = "5.0.0"
+version = "5.0.1"
 
 dependencies {
     constraints {
         api("io.springfox:springfox-boot-starter:3.0.0")
         api("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
-        api("io.github.microutils:kotlin-logging:1.8.3")
+        api("io.github.microutils:kotlin-logging:1.12.0")
         api("com.h2database:h2:1.4.200")
     }
-    api(platform("org.springframework.boot:spring-boot-dependencies:2.3.3.RELEASE"))
+    api(platform("org.springframework.boot:spring-boot-dependencies:2.3.4.RELEASE"))
 }
 
 javaPlatform {
