7371141bc433dc79c6b5ea12a615b6a3309918d9
Vladimir Hromada <vhromada@gmail.com>
2021-04-03 07:45:28 +0200

    Kotlin logging 2.0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 }
 
 group = "com.github.vhromada.project"
-version = "5.0.4"
+version = "5.0.5"
 
 dependencies {
     constraints {
         api("io.springfox:springfox-boot-starter:3.0.0")
         api("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
-        api("io.github.microutils:kotlin-logging:2.0.4")
+        api("io.github.microutils:kotlin-logging:2.0.6")
         api("com.h2database:h2:1.4.200")
     }
     api(platform("org.springframework.boot:spring-boot-dependencies:2.4.4"))
